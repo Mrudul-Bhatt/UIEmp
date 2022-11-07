66a89f1a98d2de1795660fe840a679530a3150a6
Mrudul Bhatt <mrudul1203bhatt@gmail.com>
2022-11-07 06:17:04 +0000

    added search functionality and getAllEmployees

--- AllEmployees/allEmployees.js
+++ AllEmployees/allEmployees.js
@@ -1,55 +1,74 @@
 const allEmp = document.getElementById("allEmp");
-// const searchEmp = document.getElementById("searchEmp");
+const searchEmp = document.getElementById("searchEmp");
+const searchName = document.getElementsByName("search");
 // const searchInput = document.getElementById("example").querySelector("input");
 
 const getAllEmployees = () => {
-  const list = document.getElementById("employees");
+	const list = document.getElementById("employees_list");
+	list.innerHTML = "";
 
-  fetch("https://localhost:7209/api/Employees")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("GET Success:", data);
-
-      if (data.length != 0) {
-        data.forEach((element) => {
-          var html = "";
-          const newTr = document.createElement("tr");
-          html += `
-            <td>${element.name}</td>
-            <td>${element.email}</td>
-            <td>${element.dateOfBirth}</td>
-            <td>${element.salary}</td>
-            <td>${element.department}</td>
-            <td>View</td>
-          `;
-          newTr.innerHTML = html;
-          list.append(newTr);
-          // console.log(element);
-        });
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	// https://localhost:7209/api/Employees
+	fetch("https://localhost:7209/api/Employees")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("GET Success:", data);
+			data.forEach((element) => {
+				var html = "";
+				const newTr = document.createElement("tr");
+				html += `
+				<td>${element.name}</td>
+				<td>${element.email}</td>
+				<td>${element.dateOfBirth}</td>
+				<td>${element.salary}</td>
+				<td>${element.department}</td>
+				<td>View</td>
+				`;
+				newTr.innerHTML = html;
+				list.append(newTr);
+			});
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 };
 
-// const searchEmployee = () => {
-//   const searchName = searchInput.value;
+const searchEmployee = () => {
+	const list = document.getElementById("employees_list");
+	list.innerHTML = "";
 
-//   if (searchName.trim() === "") {
-//     alert("Enter a valid name");
-//     return;
-//   }
-
-//   fetch("https://localhost:7209/api/Employees")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log("GET Success:", data);
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// };
+	// https://localhost:7209/api/Employees
+	fetch("https://localhost:7209/api/Employees")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("GET Success:", data);
+			if (data.length != 0) {
+				data.forEach((element) => {
+					if (
+						element.name
+							.toLowerCase()
+							.includes(searchName[0].value.toLowerCase())
+					) {
+						var html = "";
+						const newTr = document.createElement("tr");
+						html += `
+						<td>${element.name}</td>
+						<td>${element.email}</td>
+						<td>${element.dateOfBirth}</td>
+						<td>${element.salary}</td>
+						<td>${element.department}</td>
+						<td>View</td>
+						`;
+						newTr.innerHTML = html;
+						list.append(newTr);
+					}
+				});
+			}
+			searchName[0].value = "";
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+};
 
 allEmp.addEventListener("click", getAllEmployees);
-// searchEmp.addEventListener("click", searchEmployee);
+searchEmp.addEventListener("click", searchEmployee);
